9a5aff49b1d2ff6f3ee563f2040ebda462998342
Gabrielle Noel <gabby@gabriellealexa.com>
2020-02-07 05:58:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ function theBeatlesPlay(musicians, instruments){
 
 function johnLennonFacts(facts){
   const shoutedFacts =[]
+  
   let i = 0;
   while (i < facts.length) {
     shoutedFacts.push('${facts[i]}!!!')
